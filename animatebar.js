a1a261dcea4d4f7ca54eecebea5a1cc113f5e643
jacksontheel <jackson.l.theel@gmail.com>
2022-04-11 20:57:27 -0600

    Fix animation

--- animatebar.js
+++ animatebar.js
@@ -1,9 +1,10 @@
 multiplier = 1
 
 var intervalId = window.setInterval(function(){
+    if ( document.hidden ) { return; } 
     var percent = $("#fill").width() / $("#fill").parent().width() * 100;
     $("#fill").animate({
-    width: (percent + (5 * multiplier)) + "%",
+    width: (percent + (2 * multiplier)) + "%",
     }, 1500);
     multiplier *= -1;
 }, 5000);
